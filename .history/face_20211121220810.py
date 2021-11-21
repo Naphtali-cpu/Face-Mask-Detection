7629d4a2338949cacd88190290f2868eedb0f368
Naphtali-cpu <naphtali919@gmail.com>
2021-11-21 22:09:30 +0300

    Added frame size for message

--- .history/face_20211121220810.py
+++ .history/face_20211121220810.py
@@ -69,7 +69,5 @@ while True:
 
         cv2.putText(frame, label, (startX, startY - 10),
             cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
-        cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
-
- 
+        cv2.rectangle(frame, (startX, start)) 
 
